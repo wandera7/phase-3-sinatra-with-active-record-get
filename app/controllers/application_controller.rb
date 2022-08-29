cefdb7bec5d7a140a397dbe1db420db9fff0fae0
wandera7 <julius.wandera@student.moringaschool.com>
2022-08-29 10:35:14 +0300

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  # Add this line to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
   end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    # include associated reviews in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+  end
+
 
 end
